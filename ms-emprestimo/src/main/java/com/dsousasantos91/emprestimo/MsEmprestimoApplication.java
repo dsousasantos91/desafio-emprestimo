9216139da4a67bdfe584e28a0d3485b1f7c45026
Douglas Santos <dsousasantos91@gmail.com>
2024-02-07 21:57:01 -0300

    primeiro commit - projeto inicial

---
+++ ms-emprestimo/src/main/java/com/dsousasantos91/emprestimo/MsEmprestimoApplication.java
@@ -0,0 +1,13 @@
+package com.dsousasantos91.emprestimo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsEmprestimoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsEmprestimoApplication.class, args);
+	}
+
+}
---
+++ ms-emprestimo/src/test/java/com/dsousasantos91/emprestimo/MsEmprestimoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dsousasantos91.emprestimo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsEmprestimoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
