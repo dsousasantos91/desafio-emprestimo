1498da25077121c46facada13226529a02103f53
Douglas Santos <dsousasantos91@gmail.com>
2024-02-07 23:01:56 -0300

    definir valorMinMensal e valorMaxEmprestimo para enum TipoIdentificador

--- ms-emprestimo/src/main/java/com/dsousasantos91/emprestimo/domain/enumeration/TipoIdentificador.java
+++ ms-emprestimo/src/main/java/com/dsousasantos91/emprestimo/domain/enumeration/TipoIdentificador.java
@@ -1,9 +1,22 @@
 package com.dsousasantos91.emprestimo.domain.enumeration;
 
+import lombok.Getter;
+
+import java.math.BigDecimal;
+
+@Getter
 public enum TipoIdentificador {
 
-    PF,
-    PJ,
-    AU,
-    AP
+    PF(BigDecimal.valueOf(300.0), BigDecimal.valueOf(10000.0)),
+    PJ(BigDecimal.valueOf(1000.0), BigDecimal.valueOf(100000.0)),
+    AU(BigDecimal.valueOf(100.0), BigDecimal.valueOf(10000.0)),
+    AP(BigDecimal.valueOf(400.0), BigDecimal.valueOf(25000.0));
+
+    private final BigDecimal valorMinMensal;
+    private final BigDecimal valorMaxEmprestimo;
+
+    TipoIdentificador(BigDecimal valorMinMensal, BigDecimal valorMaxEmprestimo) {
+        this.valorMinMensal = valorMinMensal;
+        this.valorMaxEmprestimo = valorMaxEmprestimo;
+    }
 }
